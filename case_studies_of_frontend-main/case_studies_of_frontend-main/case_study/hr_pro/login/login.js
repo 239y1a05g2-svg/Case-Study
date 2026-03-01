21ec6f6ff11e0c08805cd8493c05b825c0c9758c
239y1a05g2-svg <239y1a05g2@ksrmce.ac.in>
2026-03-01 16:56:12 +0530

    Update login.js

--- case_studies_of_frontend-main/case_studies_of_frontend-main/case_study/hr_pro/login/login.js
+++ case_studies_of_frontend-main/case_studies_of_frontend-main/case_study/hr_pro/login/login.js
@@ -3,11 +3,12 @@ function login(){
 var email = document.getElementById("email").value;
 var pass = document.getElementById("pass").value;
 
-if(email == "madhu@mail.com" && pass == "1234"){
+if(email == "vishnu@mail.com" && pass == "1234"){
 alert("Login Successful");
 }
 else{
 alert("Wrong Email or Password");
 }
+
 
 }
